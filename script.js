2be8a4f329a951b3d651e788760c3885bb6e2c8c
stealthninja8 <zskendall@gmail.com>
2016-11-14 12:58:23 -0500

    Update script.js for Gallery

--- script.js
+++ script.js
@@ -20,13 +20,32 @@ function fillYear() {
     document.getElementById('year').innerHTML = options;
 }
 
-function main() {
-    fillDay();
-    fillYear();
+function thumbnails() {
+    var ico = "";
+    var width = document.getElementById('big').offsetWidth;
+    console.log(width);
+    for(var i = 0; i < Math.floor(width/(75+30))*5; i++) {
+        ico += "<div class = 'icon' id = '" + i + "' onclick='showImage(this.id)'> </div>";
+    }
+
+    document.getElementById('big').innerHTML = ico;
 }
 
-function dates() {
-    
+function showImage(imgID) {
+    console.log(imgID);
+    var elem = document.getElementById(imgID);
+    // var src = elem.src;
+    // ^ or something similar
+    var enlarged = document.createElement("div");
+    enlarged.className += 'bigImage';
+    enlarged.innerHTML += "Hello, I'd like to be an image please.";
+    console.log(enlarged.className);
+    document.body.appendChild(enlarged);
+}
+
+function main() {
+    if (document.getElementById('day') != null) fillDay();
+    if (document.getElementById('year') != null) fillYear();
 }
 
 window.onload = main;
