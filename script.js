9bd7b0478204c818ff93a891c3b60724559b5ca0
Zoe Kendall <zskendall@gmail.com>
2016-11-16 16:02:14 -0500

    fix names

--- script.js
+++ script.js
@@ -32,11 +32,13 @@ function thumbnails() {
         var prefix = "0";
         if (i+1 < 10) prefix = "00";
         var filename = "images/gallery/IMG_"+prefix+(i+1)+".JPG";
+//        console.log(filename);
         ico += "<img class = 'icon' id = '" + i + "'src = " + filename + " onclick='showImage(this.id)' />";
         var image = new Image();
         image.src = filename;
         heights.push(image.height);
         widths.push(image.width);
+//        console.log(i + " " + image.width + " " + image.height);
     }
     document.getElementById('thumb').innerHTML = ico;
     document.onkeydown = function(e) {
@@ -53,22 +55,32 @@ function makeElement(type, content, classname, id) {
 }
 
 function showImage(imgID) {
-    var box;
     currentImg = parseInt(imgID);
-    var elem = document.getElementById(imgID);
-    var src = elem.getAttribute('src');
-    var calcWidth = 360*widths[imgID]/heights[imgID];
-    var displayWidth = (calcWidth+120) + "px";
+//    var calcWidth = 360*widths[imgID]/heights[imgID];
+//    var displayWidth = (calcWidth+120) + "px";
     if (document.getElementById('bigImage') == null) {
         var screen = makeElement('div', " ", null, 'screen');
         // Math.max(window.innerHeight, document.body.clientHeight);
         screen.style.width = window.innerWidth + "px";
-        screen.style.height = window.outerHeight + "px";
+//        screen.style.height = window.outerHeight + "px";
+        screen.style.height = Math.max(window.innerHeight, 
+                                       document.body.clientHeight) + "px";
         document.getElementById('big').appendChild(screen);
-        box = makeElement("div", null, null, 'expand');
+        var elem = document.getElementById(imgID);
+        var src = elem.getAttribute('src');
+        var image = new Image();
+        image.src = src;
+        console.log(image.width + " " + image.height);
+        var box = makeElement("div", null, null, 'expand');
         var enlarged = makeElement("img", null, null, 'bigImage');
         enlarged.setAttribute('src', src);
+        var calcWidth = 360*image.width/image.height;
+        console.log(calcWidth);
+        console.log(calcWidth + 120);
+        console.log(typeof(calcWidth+120));
+        var displayWidth = (calcWidth+120) + "px";
         box.style.width = displayWidth;
+        enlarged.style.width = calcWidth + "px";
         var left = makeElement("div", "<", 'switch', 'left');
         var right = makeElement("div", ">", 'switch', 'right');
         left.onclick = function() { changeImage("left"); };
@@ -78,7 +90,16 @@ function showImage(imgID) {
         box.appendChild(right);
         document.getElementById('big').appendChild(box);
     } else {
-        box = document.getElementById('expand');
+        var elem = document.getElementById(imgID);
+        var src = elem.getAttribute('src');
+        var image = new Image();
+        image.src = src;
+        console.log(image.width + " " + image.height);
+        var box = document.getElementById('expand');
+        var calcWidth = 360*image.width/image.height;
+        console.log(calcWidth);
+        console.log(calcWidth + 120);
+        var displayWidth = (calcWidth+120) + "px";
         box.style.width = displayWidth;
         document.getElementById('bigImage').setAttribute('src', src);
     }
