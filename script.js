61f05580c127891b9e27a2fe33f2f8a3591bc455
Zoe Kendall <zskendall@gmail.com>
2016-11-16 19:15:15 -0500

    hotfix on script

--- script.js
+++ script.js
@@ -32,13 +32,7 @@ function thumbnails() {
         var prefix = "0";
         if (i+1 < 10) prefix = "00";
         var filename = "images/gallery/IMG_"+prefix+(i+1)+".JPG";
-//        console.log(filename);
-        ico += "<img class = 'icon' id = '" + i + "'src = " + filename + " onclick='showImage(this.id)' />";
-        var image = new Image();
-        image.src = filename;
-        heights.push(image.height);
-        widths.push(image.width);
-//        console.log(i + " " + image.width + " " + image.height);
+        ico += "<img class = 'icon' id = '" + i + "'src = " + filename + " onclick='enlarge(this.id)' />";
     }
     document.getElementById('thumb').innerHTML = ico;
     document.onkeydown = function(e) {
@@ -54,55 +48,69 @@ function makeElement(type, content, classname, id) {
     return elem;
 }
 
+function enlarge(imgID) {
+    for(var j = 0; j < 31; j++) {
+        var elem = document.getElementById('' + j);
+        var zimg = new Image();
+        zimg.src = elem.getAttribute('src');
+        heights.push(zimg.height);
+        widths.push(zimg.width);
+    }
+    showImage(imgID);
+}
+
 function showImage(imgID) {
     currentImg = parseInt(imgID);
-//    var calcWidth = 360*widths[imgID]/heights[imgID];
-//    var displayWidth = (calcWidth+120) + "px";
+    var box;
+    var src = document.getElementById(imgID).getAttribute('src');
+    var calcWidth = 360*widths[imgID]/heights[imgID];
+    var displayWidth = (calcWidth + 120) + "px";
     if (document.getElementById('bigImage') == null) {
-        var screen = makeElement('div', " ", null, 'screen');
-        // Math.max(window.innerHeight, document.body.clientHeight);
+        // darken background
+        var screen = makeElement("div", " ", null, 'screen');
         screen.style.width = window.innerWidth + "px";
-//        screen.style.height = window.outerHeight + "px";
-        screen.style.height = Math.max(window.innerHeight, 
-                                       document.body.clientHeight) + "px";
+        screen.style.height = Math.max(document.body.clientHeight,
+                                      window.innerHeight) + "px";
+        screen.onclick = function() { closeImage(); }
         document.getElementById('big').appendChild(screen);
-        var elem = document.getElementById(imgID);
-        var src = elem.getAttribute('src');
-        var image = new Image();
-        image.src = src;
-        console.log(image.width + " " + image.height);
-        var box = makeElement("div", null, null, 'expand');
+        
+        // create new large image of correct source
+        console.log(src);
+        console.log(document.getElementById(imgID).height);
+        box = makeElement("div", null, null, 'expand');
         var enlarged = makeElement("img", null, null, 'bigImage');
         enlarged.setAttribute('src', src);
-        var calcWidth = 360*image.width/image.height;
-        console.log(calcWidth);
-        console.log(calcWidth + 120);
-        console.log(typeof(calcWidth+120));
-        var displayWidth = (calcWidth+120) + "px";
         box.style.width = displayWidth;
-        enlarged.style.width = calcWidth + "px";
+        
+        // make navigation bars
         var left = makeElement("div", "<", 'switch', 'left');
         var right = makeElement("div", ">", 'switch', 'right');
         left.onclick = function() { changeImage("left"); };
         right.onclick = function() { changeImage('right'); };
+        
+        // setup and add the enlarged image with navigation
         box.appendChild(left);
         box.appendChild(enlarged);
         box.appendChild(right);
         document.getElementById('big').appendChild(box);
     } else {
-        var elem = document.getElementById(imgID);
-        var src = elem.getAttribute('src');
-        var image = new Image();
-        image.src = src;
-        console.log(image.width + " " + image.height);
-        var box = document.getElementById('expand');
-        var calcWidth = 360*image.width/image.height;
-        console.log(calcWidth);
-        console.log(calcWidth + 120);
-        var displayWidth = (calcWidth+120) + "px";
+        // if the expansion box was hidden, unhide it
+        if (document.getElementById('expand').hidden) {
+            document.getElementById('expand').hidden = false;
+            document.getElementById('expand').style.visibility = 'visible';
+            document.getElementById('screen').hidden = false;
+        }
+        // just switch the image out and resize the container
+        box = document.getElementById('expand');
         box.style.width = displayWidth;
         document.getElementById('bigImage').setAttribute('src', src);
     }
+}
+
+function closeImage() {
+    document.getElementById('expand').hidden = true;
+    document.getElementById('expand').style.visibility = 'hidden';
+    document.getElementById('screen').hidden = true;
 }
 
 function changeImage(dir) {
