b296cce2b1c8035545872a89dc633c0b25cd15b7
Zoe Kendall <zskendall@gmail.com>
2016-11-18 12:19:06 -0500

    add slideshow

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // script.js
+
+// global vars
 var heights = [];
 var widths = [];
 var currentImg;
 var lastImg = 30;
+var currentSlide;
+var numSlides;
+var images;
 
 function fillDay() {
     var start = 1;
@@ -130,9 +135,27 @@ function switchImage(e) {
     }
 }
 
+function changeSlide(imgID) {
+    currentSlide = (currentSlide + 1) % numSlides;
+    document.getElementById(imgID).src = images[currentSlide];
+}
+
 function main() {
     if (document.getElementById('day') != null) fillDay();
     if (document.getElementById('year') != null) fillYear();
+    
+    // prepare images for slideshow
+    images = new Array("images/IMG_002.JPG", "images/IMG_009.JPG", "images/IMG_016.JPG", "images/IMG_020.JPG", "images/IMG_021.JPG", "images/IMG_031.JPG", "images/IMG_039.JPG");
+    
+    numSlides = images.length;
+    currentSlide = images.length-1;
+    
+    var preloads = new Array(numSlides);
+    for (var i = 0; i < numSlides; i++) {
+        preloads[i] = new Image();
+        preloads[i].src = images[i];
+        preloads[i].id = 'slide';
+    }
 }
 
 window.onload = main;
